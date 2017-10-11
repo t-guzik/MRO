d3bf7b8c5963c49fb1145d0f8f469ef5ac20b870
Tomasz <tomasz.guzik94@gmail.com>
2017-10-12 00:50:25 +0200

    Enhancing performance

--- mro1.js
+++ mro1.js
@@ -3,12 +3,41 @@ import fs from 'fs';
 import _ from 'lodash';
 import chalk from 'chalk';
 import now from 'performance-now';
+import moment from 'moment';
 
 // Logging
 const log = console.log;
+const padding = 18;
 const info = text => log(chalk.bold.blue(text))
-const result = text => log('Result: '  + chalk.green(text) + '\n')
+const result = text => log(chalk.green('Result: '.padding(padding) + text))
+const time = text => log(chalk.red('Execution time: '.padding(padding) + text + 'ms\n'))
+const totalTime = text => log(chalk.redBright('Total execution time: '.padding(padding) + text + 'ms\n'))
 
+// Utils
+String.prototype.padding = function(n, c) {
+  let val = this.valueOf();
+  if ( Math.abs(n) <= val.length ) {
+    return val;
+  }
+
+  let m = Math.max((Math.abs(n) - this.length) || 0, 0);
+  let pad = new Array(m + 1).join(String(c || ' ').charAt(0));
+
+  return (n < 0) ? pad + val : val + pad;
+};
+
+const totalTimeMsg = '###total###';
+
+const measurePerformance = (fun, ...args) => {
+  const start = now()
+  const result = fun(args)
+  const end = now()
+  const measuredTime = (end-start)
+
+  args[0] !== totalTimeMsg ? time(measuredTime.toPrecision(5)) : totalTime(measuredTime.toPrecision(5))
+
+  return result;
+}
 
 // Data
 const radius = 10;
@@ -21,29 +50,36 @@ const pointsNumber = 1000;
 
 const dimension = {
   initial : 2,
-  max     : 18
+  max     : 13
 }
 
-const PRECISION = 5;
+const resultPrecision = 3;
 
 // Functions
 const euclidianDistance = (args) => {
-  const differences = _(args)
-    .map(a => math.square(radius - a))
-    .reduce((sum, n) => sum + n, 0)
+  let differences = 0;
+
+  for(let i = 0; i < args.length; i++) {
+    differences += math.square(radius - args[i])
+  }
 
   return math.sqrt(differences)
 }
 
-for(let i = dimension.initial; i < dimension.max; i++) {
+const calculateCurseOfDimension = dimension => {
   let inside = 0;
-  const pointsForDimension = pointsNumber * (math.pow(2, (i-1)));
-  info(`Dimension: ${i} Points: ${pointsForDimension}`)
+  const pointsForDimension = pointsNumber * (math.pow(2, (dimension-1)));
+  info(`Dimension: ${dimension} | Points: ${pointsForDimension}`)
 
   for (let j = 0; j < pointsForDimension; j++) {
-    if(euclidianDistance(math.bignumber(math.random([i], point.min, point.max))) < radius) {
+    if(euclidianDistance(math.random([dimension], point.min, point.max)) < radius) {
       inside++
     }
   }
-  result(((inside/pointsForDimension) * 100).toFixed(PRECISION) + '%')
+  result(((inside/pointsForDimension) * 100).toPrecision(resultPrecision) + '%')
 }
+measurePerformance( () => {
+  for(let i = dimension.initial; i < dimension.max; i++) {
+    measurePerformance(calculateCurseOfDimension, i)
+  }
+}, totalTimeMsg)
